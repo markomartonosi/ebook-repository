ab64f20f23990b623675c7f73491588becd9de4e
marko <marko.martonosi@gmail.com>
2019-09-17 15:06:12 +0200

    Add wait_for_db
    Created django command that checks database connectivity before running server/migrations.
    Removed excess mysql service environment variables to avoid duplicates of configuration : https://github.com/docker-library/mysql/issues/129

---
+++ backbone/ebook_repository/management/commands/wait_for_db.py
@@ -0,0 +1,30 @@
+import time
+
+import MySQLdb
+from django.core.management import BaseCommand
+from django.db import connections, DEFAULT_DB_ALIAS
+from MySQLdb._exceptions import OperationalError as mysql_operational_error
+from django.db.utils import OperationalError as django_operational_error
+
+
+class Command(BaseCommand):
+    """
+    Executed before running database migrations to ensure database service is actually
+    up and running, in other case retries connecting after 3 seconds. Currently only
+    supports mysql.
+    """
+    def handle(self, *args, **options):
+        db_conn = None
+        while not db_conn:
+            try:
+                default_database = connections.databases[DEFAULT_DB_ALIAS]
+                db_conn = MySQLdb.connect(
+                    default_database["HOST"],
+                    default_database["USER"],
+                    default_database["PASSWORD"],
+                    default_database["NAME"]
+                )
+                self.stdout.write("Connection to database service established!")
+            except (django_operational_error, mysql_operational_error):
+                self.stdout.write("Database service is down. Retrying to connect in 10 sec ...")  # noqa E501
+                time.sleep(10)
